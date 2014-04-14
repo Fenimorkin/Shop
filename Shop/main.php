1963dfdd1a36cd73ee2b8f65a337f5651ca84707
Fenimorkin <vniia@gmail.com>
2014-04-15 01:04:28 +0400

    Часть 1  Основы PHP Глава 4 ООП Полиморфизм: Одинаковое поведение классов
    Листинг 4-1 Иерархия классов для продуктов

--- Shop/main.php
+++ Shop/main.php
@@ -1,7 +1,7 @@
 <?php
  abstract class Product
  {
- 	protected $id;
+ 	protected $pid;
  	protected $name;
  	protected $desc;
  	protected $price_per_unit;
@@ -29,6 +29,57 @@
  abstract public function get_number_in_stock($in_num_desired);
  abstract public function ship_product_units($in_num_shipped);
  
- 
- 	
+ public function get_ProductID()
+ {
+ 	return  $this->pid;
  }
+ 
+ public function get_Name()
+ {
+ 	return  $this->name;
+ }
+ 
+ public function get_Description()
+ {
+ 	return  $this->desc;
+ }
+ 
+ public function get_PricePerUnit()
+ {
+ 	return  $this->price_per_unit;
+ }
+ 
+ }
+ 
+ class LocalProduct extends Product
+ {
+ 	public function get_number_in_stock($in_num_desired)
+ 	{
+ 		//go to our local DBs and seehow many we have left
+ 		//return -1 on a failure of some sort
+ 	}
+ 	
+ 	public function ship_product_units($in_num_shipped)
+ 	{
+ 		//go to our localdbs and mark $in_number units as no
+ 		//longer available. TRUE == success, FALSE == failure
+ 	}
+ }
+ 
+ class NavigationPartnerProduct extends  Product
+ {
+ 	public function get_number_in_stock($in_num_desired)
+ 	{
+ 		//go to our navigation equipment partner`s servers
+ 		//and see how many are left/ Return -1 on failure 
+ 	}
+ 	
+ 	public function ship_product_units($in_num_shipped)
+ 	{
+ 		//go to our navigation equipment partner`s servers
+ 		//and mark $in_numbers units as no longer available
+ 		//Return FALSE on failure, TRUE on success
+ 	}
+ }
+ 
+ ?>
